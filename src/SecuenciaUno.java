0b177c2dba8675892bb012cda2883bf6213c83bf
KatherineDominguez30 <105589656+KatherineDominguez30@users.noreply.github.com>
2024-02-27 20:18:05 -0400

    SecuenciaUno con funciones

--- src/SecuenciaUno.java
+++ src/SecuenciaUno.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
+
 public class SecuenciaUno {
 
     public static void main(String[] args){
+       int num1, num2 , res, pro;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa valor 1: ");
+        num1= scanner.nextInt();
+        System.out.print("Ingresa valor 2: ");
+        num2= scanner.nextInt();
+
+        res = SumaEnSecuencia(num1,num2);
+        System.out.println("La suma es: " +res);
+        pro = ProductoEnSecuencia(num1,num2);
+        System.out.println("\n El producto es: " +pro);
+        scanner.close();
     }
 
-
+    public static int SumaEnSecuencia(int num1, int num2){
+    return num1 + num2;
+    }
+    public static int ProductoEnSecuencia(int num1, int num2){
+        return num1 * num2;
+    }
 
 
 
