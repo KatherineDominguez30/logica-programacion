5530abbe8e0eb4f01212569a81e1cf694c1ee8c9
KatherineDominguez30 <105589656+KatherineDominguez30@users.noreply.github.com>
2024-03-02 14:35:56 -0400

    Condicion Anidada 3

---
+++ src/CondicionAnidada3.java
@@ -0,0 +1,26 @@
+/* Problema: Se ingresa por teclado un valor entero, moostrar una leyenda que indique si el numero
+* es positivo, negativo o nulo(es decir cero)*/
+import java.util.Scanner;
+public class CondicionAnidada3 {
+    public static void main(String[] args){
+        funcionCondicionAnidada2();
+    }
+
+
+
+    public static void funcionCondicionAnidada2(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un valor: ");
+        int num1 = scanner.nextInt();
+
+        if (num1 > 0){
+            System.out.print("El numero es positivo");
+        }  else if (num1 == 0) {
+            System.out.print("El numero es nulo");
+        } else {
+            System.out.print("El numero es negativo");
+        }
+
+        scanner.close();
+    }
+}
